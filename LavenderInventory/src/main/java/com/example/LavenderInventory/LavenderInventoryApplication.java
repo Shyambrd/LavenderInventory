ec927e98ee65dffef18cd5d9cfd16c28bcd2a28c
Shyam <Shyambrd78@gmail.com>
2023-01-17 23:43:37 +0530

    New Project

---
+++ LavenderInventory/src/main/java/com/example/LavenderInventory/LavenderInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.LavenderInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavenderInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavenderInventoryApplication.class, args);
+	}
+
+}
---
+++ LavenderInventory/src/test/java/com/example/LavenderInventory/LavenderInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LavenderInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavenderInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
