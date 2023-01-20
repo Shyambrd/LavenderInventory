f25bac2c986f67b1b225bc847f831cbd90fe5b70
Shyam <Shyambrd78@gmail.com>
2023-01-20 23:35:06 +0530

    New product added

--- LavenderInventory/src/main/java/com/example/LavenderInventory/LavenderInventoryApplication.java
+++ LavenderInventory/src/main/java/com/example/LavenderInventory/LavenderInventoryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-
+// for configuration 
 @SpringBootApplication
 public class LavenderInventoryApplication {
 
